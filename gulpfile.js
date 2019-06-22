1453f21939c183eaf4f973498a4ff5b674f31a4b
alex <stwork87@gmail.com>
2019-06-22 12:51:30 +0300

    complited start package

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ function clean() {
 }
 
 function watch() {
-  gulp.watch(paths.src + 'scss/*.scss', styles);
+  gulp.watch(paths.src + 'scss/**/*.scss', styles);
   gulp.watch(paths.src + 'js/*.js', scripts);
   gulp.watch(paths.src + '*.html', htmls);
 }
